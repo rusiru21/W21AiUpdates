1c320f9ab5caf20dc6741e34a6753aec151755b2
rusiru21 <rusirunishanta45@gmail.com>
2025-08-06 22:00:51 +0400

    Added dark mode command script

---
+++ dark_mode.py
@@ -0,0 +1,13 @@
+window.setStyleSheet("""
+    QWidget {
+        background-color: black;
+        color: white;
+        font-size: 14px;
+    }
+    QLineEdit, QPushButton {
+        background-color: #222;
+        color: white;
+        border: 1px solid white;
+    }
+""")
+
